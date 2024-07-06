15b7533ac7d01db7a58b2e62bc5eb82d2c1a1085
ElMehdiTafnil <elmehdi.tafnil@gmail.com>
2024-07-06 16:07:08 +0100

    set up index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+puts "cleaning the daabase..."
+Girlfriend.destroy_all
+puts "seeding a database..."
+
+
+puts "seeding a database..."
+20.times do
+  Girlfriend.create(
+    name: Faker::Name.name,
+    location: Faker::Address.city,
+    hourly_rate: Faker::Number.between(from: 10, to: 50),
+    availability: Faker::Boolean.boolean,
+    bio: Faker::Lorem.paragraph,
+    user_id: 1,
+  )
+end
+puts "seeding completed!"
+puts "you have #{Girlfriend.count} girlfriends in your database!"
